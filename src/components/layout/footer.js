fb7f961192f283cd63588b58c4f5b2eff60fa688
abeeken <abeeken@gmail.com>
2019-12-05 19:53:19 +0000

    Small change

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -4,7 +4,7 @@ import "./styles/footer.module.scss"
 export default () => (
     <footer>
         <p>&copy; Copyright 2019</p>
-        <p>The Fantastic Site Inc</p>
+        <p>The Fantastic Site Inc.</p>
         <p>Links here</p>
     </footer>
 )
