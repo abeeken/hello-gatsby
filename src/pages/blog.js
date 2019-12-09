ca8ca504e0ecf3e750767e67e9977a30e3836ef0
Andrew Beeken <abeeken@gmail.com>
2019-12-09 15:37:56 +0000

    Blog posts list

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,35 +2,49 @@ import React from "react"
 import { graphql } from "gatsby"
 import Content from "../components/layout/content"
 import { Helmet } from "react-helmet"
-import PostLink from "../components/post-link"
 
-const BlogPage = ({
-    data: {
-        allMarkdownRemark: { edges },
-    },
-}) => {
-    const Posts = edges
-        .filter(edge => !!edge.node.frontmatter.date) // Filter posts
-        .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
-return <Content><Helmet><title>The Fantastic Site | Blog</title>{Posts}</Helmet></Content>
+const BlogPage = ({data}) => {
+    const {edges} = data.allMarkdownRemark
+    console.log(edges)
+    return(
+        <Content>
+            <Helmet>
+                <title>The Fantastic Site | Blog</title>
+            </Helmet>
+            <div class="page blog">
+                <h2>Blog</h2>
+                {edges.map(edge => {
+                    const {frontmatter} = edge.node
+                    return(
+                        <div class="post">
+                            <h3 key={frontmatter.path}><a href={frontmatter.path}>{frontmatter.title}</a></h3>
+                            <p>{frontmatter.date}</p>
+                            <p>{edge.node.excerpt}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        </Content>
+    )
 }
 
 export default BlogPage
 
-export const pageQuery = graphql`
-  query {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          id
-          excerpt(pruneLength: 250)
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            path
-            title
-          }
+export const query = graphql`
+    query {
+        allMarkdownRemark (
+            sort: {order: DESC, fields: [frontmatter___date]}
+        ) {
+            edges {
+                node {
+                    excerpt(pruneLength: 250)
+                    frontmatter {
+                        date(formatString: "MMMM DD, YYYY")
+                        title
+                        path
+                    }
+                }
+            }
         }
-      }
     }
-  }
 `
